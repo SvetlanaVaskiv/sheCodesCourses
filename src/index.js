1092eff3719c16e880e04a49f54c7356ac46c67e
SvetlanaVaskiv <svetanochka@gmail.com>
2022-11-06 16:34:01 +0100

    feat: add API calls for forecast

--- src/index.js
+++ src/index.js
@@ -4,41 +4,40 @@ const displayedDesc = document.querySelector(".description");
 const temperature = document.getElementsByClassName("temp");
 const humidity = document.getElementsByClassName("humidity");
 const getLocalWeatherBtn = document.querySelector("#btn-currentWeather");
-let fahrenheit;
 let currentHumidity;
 let temp;
 
 const form = document.querySelector("#search-form");
 const gotCity = document.querySelector("#city");
-const celsiumLink = document.querySelector("#celsium");
-const fahrenheitLink = document.querySelector("#fahrenheit");
 const wind = document.querySelector("#wind");
 const newNameOfCity = document.querySelector("#name-of-city");
 const loading = document.querySelector("#loading");
 const iconElement = document.querySelector("#icon");
 const sunriseElement = document.querySelector("#sunrise");
 const sunsetElement = document.querySelector("#sunset");
+
+showLocalWeather();
+
 function showWeather(res) {
   if (res) {
     loading.classList.add("hide");
     iconElement.classList.remove("hide");
   }
-  console.log(res);
 
-const sunrise = res.data.sys.sunrise;
-const sunset = res.data.sys.sunset;
-const sunriseData = new Date(sunrise * 1000).toLocaleTimeString();
-const sunsetData = new Date(sunset * 1000).toLocaleTimeString();
+  const sunrise = res.data.sys.sunrise;
+  const sunset = res.data.sys.sunset;
+  const sunriseData = new Date(sunrise * 1000).toLocaleTimeString();
+  const sunsetData = new Date(sunset * 1000).toLocaleTimeString();
   currentHumidity = res.data.main.humidity;
   temp = Math.round(res.data.main.temp);
-  let speedOfWind = res.data.wind.speed;
+  let speedOfWind = Math.round(res.data.wind.speed);
   let description = res.data.weather[0].description;
   let city = res.data.name;
   sunriseElement.innerHTML = `Sunrise: ☀️ ${sunriseData}`;
   sunsetElement.innerHTML = `Sunset: 🌑 ${sunsetData}`;
   temperature[0].innerHTML = `${temp}`;
   humidity[0].innerHTML = `Humidity: ${currentHumidity} %`;
-  wind.innerHTML = `Wind: ${speedOfWind}km/h`;
+  wind.innerHTML = `Wind: ${speedOfWind} km/h`;
   displayedDesc.innerHTML = `${description}`;
   newNameOfCity.innerHTML = `${city}`;
   iconElement.setAttribute(
@@ -48,38 +47,61 @@ const sunsetData = new Date(sunset * 1000).toLocaleTimeString();
 }
 const writeCurentTemp = (event) => {
   event.preventDefault();
-
   if (gotCity.value) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${gotCity.value}&appid=${apiKey}&&units=metric`;
-    axios.get(apiUrl).then(showWeather);
+    axios.get(apiUrl).then((res) => {
+      showWeather(res);
+      const dailyApiUrl = `
+https://api.openweathermap.org/data/2.5/onecall?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&appid=${apiKey}&&units=metric`;
+
+      axios.get(dailyApiUrl).then(showForecast);
+    });
   }
+  event.target.reset();
 };
+function showForecast(res) {
+  const forecastElement = document.querySelector("#weather-forecast");
+  const forecast = res.data.daily;
+  let forecastHtml = `<div class="row">`;
+
+  forecast.forEach((forecastDay, index) => {
+    if (index < 6) {
+      forecastHtml += `     <div class="col-2">
+              <div>${new Date(forecastDay.dt * 1000)
+                .toDateString()
+                .slice(0, 10)}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="Clear" id="iconForecast" />
+              <div class="forecast-temp">
+                <span class="forecast-temp-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="forecast-temp-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span></div></div>`;
+    }
+  });
+
+  forecastHtml += `</div>`;
+
+  forecastElement.innerHTML = forecastHtml;
+}
 form.addEventListener("submit", writeCurentTemp);
-const changeTemptoCelsium = (event) => {
-  event.preventDefault();
-  celsiumLink.classList.add("pressed");
-  fahrenheitLink.classList.remove("pressed");
-  temperature[0].innerHTML = `${temp}`;
-};
-const changeTemptoFahrenheit = (event) => {
-  event.preventDefault();
-  fahrenheitLink.classList.add("pressed");
-  celsiumLink.classList.remove("pressed");
-  fahrenheit = Math.round((temp * 9) / 5 + 32);
-  temperature[0].innerHTML = `${fahrenheit}`;
-};
+
 function showLocation(position) {
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
+  const dailyLocalApiUrl = `
+https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&&units=metric`;
+
+  axios.get(dailyLocalApiUrl).then(showForecast);
 }
 
 function showLocalWeather() {
   navigator.geolocation.getCurrentPosition(showLocation);
 }
-showLocalWeather();
 
-celsiumLink.addEventListener("click", changeTemptoCelsium);
-fahrenheitLink.addEventListener("click", changeTemptoFahrenheit);
 getLocalWeatherBtn.addEventListener("click", showLocalWeather);
